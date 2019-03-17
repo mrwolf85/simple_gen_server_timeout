83710857b6a6273f7c38cad200e3e9abc1c55c04
Riccardo Degan <riccardo.degan@gmail.com>
2019-03-17 09:34:42 +0100

    Added stop_timer function

--- lib/simple_gen_server_timeout.ex
+++ lib/simple_gen_server_timeout.ex
@@ -2,9 +2,12 @@ defmodule SimpleGenServerTimeout do
   use GenServer
 
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args)
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
-
+  
+  def stop_timer() do
+    GenServer.cast(__MODULE__, :stop_timer)
+  end
   # Callbacks
 
   @impl true
@@ -18,6 +21,15 @@ defmodule SimpleGenServerTimeout do
     IO.puts("Timeout called #{DateTime.utc_now()}")
     ref = Process.send_after(self(), :timeout, 10_000)
     new_state = Map.put(state, :ref, ref)
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_cast(:stop_timer, state) do
+    ref = Map.get(state, :ref)
+    Process.cancel_timer(ref)
+    IO.puts "The timer is canceled"
+    new_state = Map.put(state, :ref, nil)
     {:noreply, new_state}
   end
 end
